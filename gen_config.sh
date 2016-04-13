9854bc7415f7e9c29b1187adb8a4fc57480ccc85
Brandon Cook <bcook@adobe.com>
2016-04-13 12:40:33 -0700

    lab-00

--- gen_config.sh
+++ gen_config.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
-git checkout lab-00
+git checkout master
 
 for i in `seq -f '%02g' 0 40`; do
 	git checkout "lab-$i"
 	# sed -i '' "s/lab-00/lab-$i/" .porter/config
-	sed -i '' "s/v0.62.1/v0.63.0/" .porter/config
+	# sed -i '' "s/v0.62.1/v0.63.0/" .porter/config
+	sed -i '' "s/w\.WriteHeader\(204\)/\/\/ default is a 200 response/" main.go
 	git commit -am "lab-$i"
 done
 
-git checkout lab-00
+git checkout master
